da0618f2e9722913369d780c6dcf97a858203c60
prachi <er.prachigupta@gmail.com>
2021-11-30 23:22:42 -0700

    Update MetadataDaoImpl.java

--- src/main/java/com/assignment/metadataapi/repository/MetadataDaoImpl.java
+++ src/main/java/com/assignment/metadataapi/repository/MetadataDaoImpl.java
@@ -7,6 +7,13 @@ import org.springframework.stereotype.Repository;
 import javax.annotation.Resource;
 import java.util.Map;
 
+/*
+Class: MetadataDaoImpl
+Description: Repository class that takes input from the user and
+calls the redis to read and write the request and return the response.
+Author:Prachi Gupta
+ */
+
 @Repository
 public class MetadataDaoImpl implements IMetadataDao{
 
@@ -16,24 +23,48 @@ public class MetadataDaoImpl implements IMetadataDao{
     @Resource(name="redisTemplate")
     private HashOperations<String, String, Metadata> hashOperations;
 
+    /*
+        Method: addMetadata
+        Params: Metadata
+        Description: Adds the metadata resource in redis
+     */
     @Override
     public void addMetadata(Metadata metadata){
         hashOperations.putIfAbsent(hashMetaData, metadata.getTitle(), metadata);
     }
+     /*
+        Method: getMetadata
+        Params: String
+        Description: Gets the metadata resource from redis
+     */
     @Override
     public Metadata getMetadata(String title){
         return hashOperations.get(hashMetaData, title);
     }
+    /*
+        Method: updateMetadata
+        Params: Metadata
+        Description: Updates the metadata resource in redis
+     */
     @Override
     public void updateMetadata(Metadata metadata) {
         hashOperations.put(hashMetaData, metadata.getTitle(), metadata);
     }
-
+    /*
+        Method: getAllMetadata
+        Params: NA
+        Description: Gets all the metadata resource from redis
+     */
     @Override
     public Map<String, Metadata> getAllMetadata() {
         return hashOperations.entries(hashMetaData);
     }
 
+     /*
+        Method: deleteMetadata
+        Params: String
+        Description: Deletes the metadata resource from redis
+     */
     @Override
     public void deleteMetadata(String title) {
         hashOperations.delete(hashMetaData, title);
