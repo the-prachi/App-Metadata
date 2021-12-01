3da1dc04dd94a2996aa30fe960e04c67761e32e0
prachi <er.prachigupta@gmail.com>
2021-11-30 23:25:45 -0700

    Update YamlJackson2HttpMessageConverter.java

--- src/main/java/com/assignment/metadataapi/config/YamlJackson2HttpMessageConverter.java
+++ src/main/java/com/assignment/metadataapi/config/YamlJackson2HttpMessageConverter.java
@@ -4,6 +4,11 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
 
+/*
+Class: YamlJackson2HttpMessageConverter
+Description: Config class to configure yaml as valid media type
+Author:Prachi Gupta
+ */
 
 public class YamlJackson2HttpMessageConverter<T> extends AbstractJackson2HttpMessageConverter {
     public YamlJackson2HttpMessageConverter () {
